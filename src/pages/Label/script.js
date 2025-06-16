1bafd6f74f3b4ea0537a0aa7a02ef836c272ab85
EmilySouza22 <emilyseafonso@gmail.com>
2025-06-16 02:09:54 -0300

    WIP: Adiciona modal 'Mais Opções'

---
+++ src/pages/Label/script.js
@@ -0,0 +1,12 @@
+/* MODAL */
+
+const openButtons = document.querySelectorAll('.btnConfig2');
+
+openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+
+        modal.showModal();
+    })
+});
