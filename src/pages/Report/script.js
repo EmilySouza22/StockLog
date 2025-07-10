f89044a651f4eb84771b6590e6e38c0974cf4631
ALICE DE OLIVEIRA COTRIM <alyceoliveira0717@gmal.com>
2025-07-09 21:08:21 -0300

    'Alterações

---
+++ src/pages/Report/script.js
@@ -0,0 +1,26 @@
+
+document.getElementById("filter").Value
+
+/* // Capturar o elemento select
+const selectElement = document.getElementById('filter');
+
+        // Adicionar um ouvinte de evento para quando a seleção mudar
+ selectElement.addEventListener('change', function() {
+            // Capturar o valor selecionado
+ const selectedValue = selectElement.value;
+
+            // Aqui você pode fazer alguma ação com o valor selecionado
+            console.log('Você selecionou:', selectedValue);
+
+            // Exemplo de ação: mostrar um alerta
+            alert('Você selecionou: ' + selectedValue);
+            
+            // Se quiser adicionar condições e filtrar, pode colocar algo assim:
+ if (selectedValue === 'Data') {
+                // Lógica para filtrar por Data
+                console.log('Filtrando por Data...');
+ } else if (selectedValue === 'Hora') {
+                // Lógica para filtrar por Hora
+                console.log('Filtrando por Hora...');
+ }
+            // E assim por diante para as outras opções */
